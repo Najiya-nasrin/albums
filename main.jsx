66651dbb6d7eebc835f9f31578f3c43bf48dcd75
Najiya Nasrin <mails4najiya@gmail.com>
2025-01-16 01:05:08 +0400

    check

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { Provider } from "./context/books.jsx";
+import App from "./src/App.jsx";
+import "./src/index.css";
+import { Provider } from "./src/context/books.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
